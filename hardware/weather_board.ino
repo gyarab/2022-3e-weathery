c0752e4d8e488ebc9fe64bc8346c8200130984e1
ondrej-salat <78552892+ondrej-salat@users.noreply.github.com>
2022-09-25 20:18:36 +0200

    initial D

---
+++ hardware/weather_board.ino
@@ -0,0 +1,52 @@
+#include <ESP8266WiFi.h>
+
+const char* nameWifi = "name";
+const char* passWifi = "password";
+const char* server = "0.0.0.0";
+String data = "";
+WiFiClient client;
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(nameWifi, passWifi);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to Wifi ");
+  Serial.println(nameWifi);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void sendData(String info) {
+  if (client.connect(server, 8000)) {
+    String message = "{\"message\": \"";
+    message += info;
+    //message.remove(zprava.length()-1,2);
+    message += "\"}";
+    Serial.println(message);
+    client.println("POST /post_data HTTP/1.1");
+    client.print("Host: ");
+    client.println(server);
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(message.length());
+    client.println();
+    client.print(message);
+  }
+  client.stop();
+  Serial.println("Data sent");
+}
+
+void loop() {
+  data = Serial.readString();
+  if (data != "") {
+    data.remove(data.length() - 1, 2);
+    Serial.print(data);
+    sendData(data);
+  }
+}
