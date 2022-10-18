e04b5144f962bd3cefd34604dd7fee4f69661cf1
Firu <filip.ruzicka@student.gyarab.cz>
2022-10-18 20:41:13 +0200

    nic

--- frontend/src/components/Mapa.vue
+++ frontend/src/components/Mapa.vue
@@ -9,6 +9,7 @@
 import L from 'leaflet'
 import '../assets/leaflet-providers.js'
 
+// tady info: https://github.com/leaflet-extras/leaflet-providers
 var map = L.map('map').setView([50.0835494, 14.4341414], 11); //praha 50.0835494N, 14.4341414E
 /*L.tileLayer.provider('Jawg.Light', {
     accessToken: 'nWDyoQqkjWUxatUXlNdKTqJymEWIFq0glO5O4kk5Ir3pUsmGb45lqJaHRyLz1OSo'
