c0fe1ecbe0fb881d152b802f998c8ca095f7ba6b
ondrej-salat <78552892+ondrej-salat@users.noreply.github.com>
2022-11-29 11:16:46 +0100

    backend TODO

--- backend/main.py
+++ backend/main.py
@@ -4,25 +4,25 @@ from models import *
 app = FastAPI()
 
 
-#TODO: vrati GPS vsech stanic
+# TODO: vrati GPS vsech stanic
 @app.get("/api/all_stations")
 async def all_stations():
     return {"message": ""}
 
 
-#TODO: vrati aktualni pocasi ze stanice s danymi GPS 
+# TODO: vrati aktualni pocasi ze stanice s danymi GPS 
 @app.get("/api/now/{gps}")
 async def now(gps: str):
     return {"message": ""}
 
 
-#TODO: vrati vsechna namerena data ze stanice s danymi GPS
+# TODO: vrati vsechna namerena data ze stanice s danymi GPS
 @app.get("/api/stats/{gps}")
 async def stats(gps: str):
     return {"message": ""}
 
 
-#TODO: (ZATIM NEDELAT) zaregistruje do databeze novou stanici s danymi GPS
+# TODO: (ZATIM NEDELAT) zaregistruje do databeze novou stanici s danymi GPS
 @app.post("/api/register_station")
 async def register_station():
     return {"message": ""}
