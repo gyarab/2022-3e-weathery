a9b42e432bacaf54b5ab46a242a3b18802ce0921
Filip Hruška <filip.hruska@student.gyarab.cz>
2022-12-16 21:24:55 +0100

    bindpopup method discovered PagMan

--- frontend/src/components/Mapa.vue
+++ frontend/src/components/Mapa.vue
@@ -28,7 +28,11 @@ L.tileLayer.provider('CartoDB.Voyager').addTo(map);
 
 L.marker([50.13378832240194, 14.378124229373308], {icon: icon}).addTo(map)
     .bindPopup('Subemelaradio <br> muj house neraidovat pls')
-L.marker([50.0742633, 14.3588950], {icon: icon}).addTo(map).bindPopup('plná taška hulení')
+var hruskq = L.marker([50.0742633, 14.3588950], {icon: icon}).addTo(map).bindPopup('plná taška hulení')
+hruskq.bindPopup
+        ("<h1>Stanice1</h1><p>Můj milovaný bodík</p><ul><li>Teplota:{{stanice1.teplota}}</li><li>Tlak: {{stanice1.tlak}}</li><li>Rychlost vzduchu: {{stanice1.rychlost_vzduchu}}</li></ul>")
+    
+
 
 onBeforeUnmount(() => {
    map.off();
