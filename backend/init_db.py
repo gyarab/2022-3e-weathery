6eb3f7d164974f552b6ee910b5692bc3edcb8915
ondrej-salat <78552892+ondrej-salat@users.noreply.github.com>
2022-12-21 15:06:51 +0100

    add message to all responses

--- backend/init_db.py
+++ backend/init_db.py
@@ -9,10 +9,11 @@ con = psycopg2.connect(
 )
 cur = con.cursor()
 cur.execute("SET timezone='UTC';")
-cur.execute("CREATE TABLE stations (gps TEXT, serial_number INT);")
 cur.execute(
     "CREATE TABLE data (gps TEXT, time TIMESTAMP, temperature INT, humidity INT, pressure INT, wind_speed INT, wind_direction TEXT, rain INT);"
 )
+cur.execute("CREATE TABLE stations (gps TEXT, serial_number INT);")
+
 cur.execute("CREATE TABLE serial_numbers (serial_number INT);")
 
 con.commit()
