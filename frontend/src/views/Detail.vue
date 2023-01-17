5c64c7e2527129f0b599994a7137ab3f33a26e36
Firu <filip.ruzicka@student.gyarab.cz>
2023-01-17 16:00:59 +0100

    prvni graf (asi switchnem libku)

--- frontend/src/views/Detail.vue
+++ frontend/src/views/Detail.vue
@@ -4,13 +4,13 @@
 
         <div id="content">
             <div id="menicko">
-                <button class="tlacitkoPrepinani" v-for="jmenoGrafu in Object.keys(grafy)" @click="aktivniGraf = jmenoGrafu">
+                <button class="tlacitkoPrepinani" v-for="jmenoGrafu in Object.keys(grafy)"
+                        @click="zmenaAktivnihoGrafu(jmenoGrafu)">
                     {{ jmenoGrafu }}
                 </button>
             </div>
 
             <Line v-if="grafData !== null" :data="grafData" :options="options"/>
-            {{ aktivniGraf }}
         </div>
     </div>
 </template>
@@ -18,58 +18,61 @@
 <script>
 import axios from "axios";
 import {Line} from 'vue-chartjs'
-import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js'
+import {Chart as ChartJS, Title, Tooltip, PointElement, LineElement, CategoryScale, LinearScale} from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip)
 
 export default {
     name: "Detail",
     components: {Line},
     data() {
         return {
-            grafy: {"Teplota": ['#ff0000', 'jmenoSouboru'], "Vlhkost": ['#003dff', 'jmenoSouboru']},
+            grafy: {Teplota: ['temperature', '#ff4d00'], Vlhkost: ['humidity', '#000dff']},
+            casoveRozmezi: "7", // dní
             aktivniGraf: "Teplota",
             souradnice: this.$route.params.souradnice.replaceAll(',', '.').split('-'),
             data: null,
-            grafData: null,
             options: {
                 responsive: true,
                 maintainAspectRatio: false
-            }
+            },
+            grafData: null,
         }
     },
     mounted() {
-        // request axios o data
         let now = new Date() // dnešek
         now.setDate(now.getDate() - 7) // týden zpátky
         axios.get("/stats/" + this.souradnice[0] + "_" + this.souradnice[1], {
             params: {
                 date_from: `${now.getDate()}-${now.getMonth() + 1}-${now.getFullYear()}`,
-                date_to: "now"
+                date_to: 'now'
             }
+        }).then(response => {
+            this.data = response.data.data
+
+            this.zmenaAktivnihoGrafu(this.aktivniGraf)
         })
-            .then(response => {
-                this.data = response.data.data
-                console.log(this.data)
-
-                this.grafData = {
-                    labels: [],
-                    datasets: []
-                }
-
-                for (let i in this.data[0]) {
-                    console.log(this.data[0][i])
-                    this.grafData.labels.push()
-                }
-
-                // {
-                //     label: this.aktivniGraf,
-                //     backgroundColor: this.grafy[this.aktivniGraf],
-                //     data: []
-                // }
-            })
 
         // zobrazení grafu pomocí https://vue-chartjs.org/
+    },
+    methods: {
+        zmenaAktivnihoGrafu(noveAktivni) {
+            this.aktivniGraf = noveAktivni
+            let grafData = {
+                labels: [],
+                datasets: [
+                    {
+                        backgroundColor: this.grafy[this.aktivniGraf][1],
+                        data: [],
+                    }
+                ]
+            }
+            for (let i in this.data) {
+                grafData.datasets[0].data.push(this.data[i][this.grafy[this.aktivniGraf][0]])
+                grafData.labels.push(this.data[i].time)
+            }
+            this.grafData = grafData
+        }
     }
 }
 </script>
@@ -93,5 +96,10 @@ export default {
     padding: 30px;
     border: 1px solid;
     background-color: white;
+}
+
+canvas {
+    max-width: 1200px;
+    height: 500px;
 }
 </style>
